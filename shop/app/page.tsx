3feab5090e4ced5e4ff72aec94f590434c877269
nowaveosu <nowaveosu@gmail.com>
2025-01-23 19:52:02 +0900

    [Add] 헤더 라우팅

--- shop/app/page.tsx
+++ shop/app/page.tsx
@@ -1,12 +1,9 @@
 
 export default function Home() {
-  //데이터 바인딩은 {}
-  //라우팅은 app/folder명/page.tsx로
-  // a 태그대신 Link사용
-  //모든 페이지에 보여야할 상단바같은 html은 layout.tsx에작성
+
   return (
     <div>
-
+      this is main page
     </div>
   );
 }
