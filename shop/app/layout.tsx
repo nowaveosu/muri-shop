cae43c88cdb83fdcf070719c0a7ee60a47b7caa9
nowaveosu <nowaveosu@gmail.com>
2025-01-24 17:19:42 +0900

    [style] 헤더 스타일링

--- shop/app/layout.tsx
+++ shop/app/layout.tsx
@@ -11,15 +11,19 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <div>
-        <Image src="/logo.png" width={300} height={64} alt="logo" />
-        </div>
-        <div>
-          <Link href="/">BEST</Link>
-          <Link href="/lotion">보습제</Link>
-          <Link href="/tonic">영양제</Link>
-          <Link href="/board">게시판</Link>
-        </div>
+        <header className="border-b-2 mb-8 border-black">
+          <div className="flex justify-center items-center mt-12">
+            <Image src="/logo.png" width={300} height={64} alt="logo" />
+          </div>
+
+          <nav className="mt-9 mb-6 flex justify-center space-x-40 text-lg">
+            <Link href="/">BEST</Link>
+            <Link href="/lotion">보습제</Link>
+            <Link href="/tonic">영양제</Link>
+            <Link href="/board">게시판</Link>
+          </nav>
+        </header>
+
         {children}
       </body>
 
