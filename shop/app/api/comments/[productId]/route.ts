8f31da5627eaba5d7c0c60e4a1994a0bb2cafac3
nowaveosu <nowaveosu@gmail.com>
2025-02-20 18:33:34 +0900

    [Add] 댓글 post 작성

--- shop/app/api/comments/[productId]/route.ts
+++ shop/app/api/comments/[productId]/route.ts
@@ -22,5 +22,31 @@ export async function GET(
     await client.close();
 
     return NextResponse.json(comments);
+}
+
+export async function POST(
+    request: NextRequest,
+    {params} : {params: {productId: string}}
+){
+    const {author, content} = await request.json();
+    const client = new MongoClient(uri);
+    await client.connect();
+
+    const db = client.db("products");
+    const collection = db.collection("comments");
+
+    const productObjectId = new ObjectId(params.productId);
+
+    const newComment = {
+        productId: productObjectId,
+        author: author || "익명",
+        content,
+        cratedAt: new Date(),
+    };
+
+    await collection.insertOne(newComment);
+    await client.close();
+
+    return NextResponse.json({ message: "댓글 작성완료"}, {status: 201});
 
 }
