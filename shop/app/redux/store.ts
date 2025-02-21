e5bdc38d82d264dd217b38bbbbc295d93941ad90
nowaveosu <nowaveosu@gmail.com>
2025-02-21 17:12:10 +0900

    [Add] 리덕스스토어 생성

---
+++ shop/app/redux/store.ts
@@ -0,0 +1,12 @@
+"use client";
+import { configureStore } from "@reduxjs/toolkit";
+import commentsReducer from "./features/commentsSlice";
+
+export const store = configureStore({
+    reducer: {
+        comments: commentsReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
