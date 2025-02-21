39baa24188b7cbdf872bbfa2fa4d7054fcda0e3d
nowaveosu <nowaveosu@gmail.com>
2025-02-21 17:31:39 +0900

    [Add] ReduxProvider 생성

--- shop/app/layout.tsx
+++ shop/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Link from "next/link";
 import Image from "next/image";
+import { ReduxProvider } from "./providers";
 
 
 export default function RootLayout({
@@ -12,20 +13,23 @@ export default function RootLayout({
     <html lang="ko"
     suppressHydrationWarning>
       <body>
-        <header className="border-b-2 mb-8 border-gray-500">
-          <div className="flex justify-center items-center mt-12">
-            <Image src="/logo.png" width={300} height={64} alt="logo" />
-          </div>
+        <ReduxProvider>
+          <header className="border-b-2 mb-8 border-gray-500">
+            <div className="flex justify-center items-center mt-12">
+              <Image src="/logo.png" width={300} height={64} alt="logo" />
+            </div>
 
-          <nav className="mt-9 mb-6 flex justify-center space-x-40 text-lg">
-            <Link href="/">BEST</Link>
-            <Link href="/lotion">보습제</Link>
-            <Link href="/tonic">영양제</Link>
-            <Link href="/board">게시판</Link>
-          </nav>
-        </header>
+            <nav className="mt-9 mb-6 flex justify-center space-x-40 text-lg">
+              <Link href="/">BEST</Link>
+              <Link href="/lotion">보습제</Link>
+              <Link href="/tonic">영양제</Link>
+              <Link href="/board">게시판</Link>
+            </nav>
+          </header>
 
-        {children}
+          {children}
+        </ReduxProvider>
+
       </body>
 
     </html>
