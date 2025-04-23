7b531e13384bde7d5c84b5b78c6b57dc2b258150
nowaveosu <nowaveosu@gmail.com>
2025-04-24 01:14:06 +0900

    [Fix] 게시글 상세 findOne 수정

--- shop/app/api/board/[id]/route.ts
+++ shop/app/api/board/[id]/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { MongoClient, ObjectId } from "mongodb";
 
 const uri = process.env.MONGODB_URI as string;
+const client = new MongoClient(uri);
+const clientPromise: Promise<MongoClient> = client.connect();
 
 export async function GET(
     request: NextRequest,
@@ -9,25 +11,33 @@ export async function GET(
         params,
     }: {
         params: Promise<{ id: string }>;
-    }) {
-
+    }
+) {
     const { id } = await params;
-    const client = new MongoClient(uri);
-    await client.connect();
 
+    const client = await clientPromise;
     const db = client.db("products");
     const collection = db.collection("board");
 
-
     const boardId = new ObjectId(id);
-    const board = await collection.findOne({ _id: boardId });
-
-    await client.close();
+    const board = await collection.findOne(
+        { _id: boardId },
+        {
+            projection: {
+                _id: 1,
+                author: 1,
+                title: 1,
+                category: 1,
+                content: 1,
+                createdAt: 1,
+            },
+        }
+    );
 
     if (!board) {
         return NextResponse.json(
-        { message: "해당 게시글이 존재하지 않습니다." },
-        { status: 404 }
+            { message: "해당 게시글이 존재하지 않습니다." },
+            { status: 404 }
         );
     }
 
