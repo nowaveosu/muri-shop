ba47ff8104e0e2d0e96be81ae311bf458defa0c7
nowaveosu <nowaveosu@gmail.com>
2025-05-14 23:46:10 +0900

    [Style] 온점제거

--- shop/app/page.tsx
+++ shop/app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
                     아토피 피부 고민, 여기서 해결하세요!
                 </h1>
                 <p className="text-lg  mb-6">
-                    아토피 피부에 좋은 제품 추천과 치료법을 공유해보세요.<br />
-                    댓글과 게시판에서 다양한 정보를 함께 나누며 피부 건강을 지켜요.
+                    아토피 피부에 좋은 제품 추천과 치료법을 공유해보세요<br />
+                    댓글과 게시판에서 다양한 정보를 함께 나누며 피부 건강을 지켜요
                 </p>
                 <div className="flex justify-center gap-4">
                     <Link href="/lotion">
@@ -45,8 +45,8 @@ export default function Home() {
                     피부관리 팁 게시판
                 </h2>
                 <p className=" max-w-xl mx-auto">
-                    아토피 피부는 건강한 생활습관과 꾸준한 보습이 중요합니다. <br />
-                    커뮤니티에서 다른 사용자들의 경험을 통해 더 나은 관리법을 찾아보세요.
+                    아토피 피부는 건강한 생활습관과 꾸준한 보습이 중요합니다 <br />
+                    커뮤니티에서 다른 사용자들의 경험을 통해 더 나은 관리법을 찾아보세요
                 </p>
             </section>
         </div>
